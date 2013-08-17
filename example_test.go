2b2b0c385d569fad6856059926e1fc230c7387c6
YoshikiShibata <yshibata@ca2.so-net.ne.jp>
2013-08-17 11:36:11 +0900

    added example

---
+++ example_test.go
@@ -0,0 +1,50 @@
+// Copyright (c) 2013 Yoshiki Shibata. All rights reserved.
+
+package xusyscall_test
+
+import "fmt"
+import . "."
+
+func Example() {
+	shmid, err := Shmget(keyOfShm, sizeOfShm, IPC_CREAT | IPC_EXCL | 0777)
+	if err != nil {
+		fmt.Printf("shmget error = " + err.Error())
+		return
+	}
+
+	defer func() {
+		err := Shmrm(shmid)
+		if err != nil {
+			fmt.Printf("Shmrd error = " + err.Error())
+			return
+		}
+		panic("panic")
+	}()
+
+	var data []byte
+	data, err = Shmat(shmid, true)
+
+	if err != nil {
+		fmt.Printf("Shmat error = " + err.Error())
+		return
+	}
+
+	defer func() {
+		err := Shmdt(data)	
+		if err != nil {
+			fmt.Printf("Shmdt error = " + err.Error())
+			return
+		}
+	}()
+
+	fmt.Printf("len(data) = %d\n", len(data))
+
+	// Write and Read
+	for i := 0; i < len(data); i++ {
+			b := byte(i & 0xff)
+		data[i] = b
+		if (data[i] != b) {
+			fmt.Printf("incorrect read\n")
+		}
+	}
+}
